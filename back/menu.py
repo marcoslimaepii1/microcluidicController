2120325abe352bba59c4e0e8b9e33cc7c86cc5ff
Marcos <marcos.blima2006@gmail.com>
2022-04-09 14:03:44 -0300

    menu adjust

--- back/menu.py
+++ back/menu.py
@@ -3,10 +3,13 @@ from PyQt5 import uic, QtWidgets
 def menuActions():
     print("teste")
 
+def closeWindow():
+    formulario.close()
 
 app = QtWidgets.QApplication([])
-formulario = uic.loadUi("./front/menu.ui")
+formulario = uic.loadUi("../front/menu.ui")
 formulario.pushButton.clicked.connect(menuActions)
 
 formulario.show()
-app.exec()
+formulario.pushButton_2.clicked.connect(closeWindow)
+app.exec() 

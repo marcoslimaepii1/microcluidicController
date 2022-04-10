b4df82c38846892e4b5a9206835d4db19d788407
Marcos <marcos.blima2006@gmail.com>
2022-04-10 16:44:56 -0300

    update video

--- back/menu.py
+++ back/menu.py
@@ -1,23 +1,34 @@
-# import cv2
+import cv2
 from PyQt5 import uic, QtWidgets
 from PyQt5.QtCore import Qt
+# import system module
+import sys
 
-def menuActions():
-    print("teste")
+# import some PyQt5 modules
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtGui import QImage
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import QTimer
 
-# def closeWindow():
-#     formulario.close()
-    
-# video = cv2.VideoCapture(0)
+# from ui_main_window import *
+def controlImage():
+        cap = cv2.VideoCapture(2)
+        viewCam(cap)
+            
 
-# while True:
-#     conectado, frame = video.read()
-#     cv2.imshow('Video', frame)
-    
-#     if cv2.waitKey(1)==ord('q'):
-#         break
-# video.release()
-# cv2.destroyAllWindows()
+def viewCam(cap):
+        # read image in BGR format
+        ret, image = cap.read()
+        # convert image to RGB format
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # get image infos
+        height, width, channel = image.shape
+        step = channel * width
+        # create QImage from image
+        qImg = QImage(image.data, width, height, step, QImage.Format_RGB888)
+        # show image in img_label
+        formulario.image_label.setPixmap(QPixmap.fromImage(qImg))
 
 def updateLabel3(value):
     formulario.label_3.setText(str(str(value)))
@@ -32,14 +43,14 @@ formulario = uic.loadUi("../front/menu.ui")
 formulario.label_3.setAlignment(Qt.AlignCenter)
 formulario.label_4.setAlignment(Qt.AlignCenter)
 
-# formulario.pushButton.clicked.connect(menuActions)
-
 formulario.show()
 formulario.horizontalSlider.valueChanged.connect(updateLabel3)
 formulario.horizontalSlider_2.valueChanged.connect(updateLabel4)
 
-# formulario.label_3.refresh()
-    
+
+# timer.timeout.connect(viewCam(cap))
+formulario.control_bt.clicked.connect(controlImage)
+
 
 # formulario.pushButton_2.clicked.connect(closeWindow)
 app.exec() 

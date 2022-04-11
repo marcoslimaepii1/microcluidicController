037aa753e6b4844b910363b78997d534c93e17db
Marcos <marcos.blima2006@gmail.com>
2022-04-11 03:15:05 -0300

    update GPIO

--- back/menu.py
+++ back/menu.py
@@ -44,6 +44,17 @@ def viewCam():
         formulario.image_label.setPixmap(QPixmap.fromImage(qImg))
 
 def updateLabel3(value):
+    
+    if formulario.radioButton.isChecked():
+       formulario.horizontalSlider.setMinimum(0)
+       formulario.horizontalSlider.setMaximum(192000)
+    elif formulario.radioButton_2.isChecked():   
+       formulario.horizontalSlider.setMinimum(0)
+       formulario.horizontalSlider.setMaximum(19) 
+    else:
+       formulario.horizontalSlider.setMinimum(0)
+       formulario.horizontalSlider.setMaximum(0) 
+           
     formulario.label_3.setText(str(str(value)))
     formulario.label_3.setAlignment(Qt.AlignCenter)
     
@@ -54,11 +65,10 @@ def updateLabel4(value):
 app = QtWidgets.QApplication([])
 formulario = uic.loadUi("../front/menu.ui")
 formulario.label_3.setAlignment(Qt.AlignCenter)
-formulario.label_4.setAlignment(Qt.AlignCenter)
 
 formulario.show()
 formulario.horizontalSlider.valueChanged.connect(updateLabel3)
-formulario.horizontalSlider_2.valueChanged.connect(updateLabel4)
+# formulario.horizontalSlider_2.valueChanged.connect(updateLabel4)
 
 timer = QTimer()
 # set timer timeout callback function
